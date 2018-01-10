18d2eabbf2de9e173cf1725a06edb59e39c8ecc1
llllllllllllllllllllllllllllllllllll <aleksander.promvalds@khk.ee>
2018-01-10 13:16:19 +0200

    2

--- aeg.php
+++ aeg.php
@@ -23,9 +23,22 @@ echo $tana.'<br />';
  * võimalusel andmete salvestamiseks kasuta sobilik
  * andmetüüp - näiteks massiivid, sõned jne
  * */
-
-echo '<form action="'.$_SERVER['PHP_SELF'].'" method="post">
-        
-    </form><hr>';
-
-
+function sisestaAndmed(){
+    echo '<form action="'.$_SERVER['PHP_SELF'].'" method="post">';
+    echo 'Päev: <input type="text" name="paev"><br />';
+    echo 'Kuu: <input type="text" name="kuu"><br />';
+    echo '<input type="submit" value="Arvuta!">';
+    echo '</form>';
+}
+function aastaKontroll($paev, $kuu){
+    $hetkeKuu = date('n');
+    $hetkePaev = date('d');
+    if($paev < $hetkePaev and $kuu == $hetkeKuu){
+        $aasta = date('Y')+1;
+    } else{
+        $aasta = date('Y');
+    }
+    return $aasta;
+}
+sisestaAndmed();
+echo aastaKontroll($_POST['paev'], $_POST['kuu']);
