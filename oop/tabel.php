6884287ebe0e118e2e6587d637c4297f33d8feec
llllllllllllllllllllllllllllllllllll <aleksander.promvalds@khk.ee>
2018-01-15 12:26:42 +0200

    loodud tabeli klass, defineeritud klassi muutujad

---
+++ oop/tabel.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aleksander
+ * Date: 15.01.2018
+ * Time: 12:23
+ */
+
+class tabel
+{
+    //klassi muutujad
+    var $tabeliSisu = array();
+    var $pealkirjad = array();
+    var $veergudeArv;
+}
