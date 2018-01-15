de66d4f13be7d50ded6e72872d04a79bbbc65138
llllllllllllllllllllllllllllllllllll <aleksander.promvalds@khk.ee>
2018-01-15 08:53:06 +0200

    klassi loomine

--- aeg.php
+++ aeg.php
@@ -39,5 +39,35 @@ function aastaKontroll($paev, $kuu){
     }
     return $aasta;
 }
+function kauaOotan($paev, $kuu, $aasta){
+    $aegHetkel = time();
+    $synniAeg = mktime(0, 0, 0, $kuu, $paev, $aasta);
+    $tulemus = array();
+    $vaheSekundites = $synniAeg - $aegHetkel;
+    $sekundid = $vaheSekundites % 60;
+    $tulemus['sekundid'] = $sekundid;
+    $vaheMinutites = $vaheSekundites / 60;
+    $minutid = $vaheMinutites % 60;
+    $tulemus['minutid'] = $minutid;
+    $vaheTundides = $vaheMinutites / 60;
+    $tunnid = $vaheTundides % 24;
+    $tulemus['tunnid'] = $tunnid;
+    $vahePaevades = $vaheTundides / 24;
+    $paevad = $vahePaevades % 30.4;
+    $tulemus['paevad'] = $paevad;
+    $kuud = $vahePaevades / 30.4;
+    settype($kuud, 'int');
+    $tulemus['kuud'] = $kuud;
+    return $tulemus;
+}
+function valjastaTulemus($andmed){
+    echo 'Sünnipäevani on: <br />';
+    foreach ($andmed as $element => $vaartus){
+        if($vaartus != 0){
+            echo $vaartus.' '.$element.'<br />';
+        }
+    }
+}
 sisestaAndmed();
-echo aastaKontroll($_POST['paev'], $_POST['kuu']);
+$tulemus = kauaOotan($_POST['paev'], $_POST['kuu'], aastaKontroll($_POST['paev'], $_POST['kuu']));
+valjastaTulemus($tulemus);
---
+++ oop/tekst.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aleksander
+ * Date: 15.01.2018
+ * Time: 8:51
+ */
+
+class tekst
+{
+
+}
